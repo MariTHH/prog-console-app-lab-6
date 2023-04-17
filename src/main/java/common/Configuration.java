e53a7213e5f4284cf5cdc94384676afe47fea9d4
Daria Shevchenko <daria.pinkfloyd@yandex.ru>
2023-04-17 17:29:39 +0300

    Add class with IP and port

---
+++ src/main/java/common/Configuration.java
@@ -0,0 +1,7 @@
+package common;
+
+public class Configuration {
+    public static final String IP = "127.0.0.1";
+    public static final int PORT = 8087;
+    //public static final String ENV_VAR = "LAB6";
+}
