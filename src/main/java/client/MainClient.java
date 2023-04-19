3507b2e29811cc87bba45dec026d55a5a27981a2
Daria Shevchenko <daria.pinkfloyd@yandex.ru>
2023-04-19 22:03:35 +0300

    made a client main in which the request is sent to the server

--- src/main/java/client/MainClient.java
+++ src/main/java/client/MainClient.java
@@ -1,4 +1,37 @@
 package client;
 
+import client.commands.CommandManager;
+import common.Configuration;
+import server.PersonCollection;
+
+import java.util.Scanner;
+
 public class MainClient {
+    private static int port = Configuration.PORT;
+
+    public static void main(String[] args) {
+        if (args.length == 1) {
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (Exception exception) {
+                System.out.println("Не получается спарсить порт.");
+            }
+        }
+
+        RequestManager requestManager = new RequestManager(port);
+        Scanner scanner = new Scanner(System.in);
+        CommandManager commandManager =new CommandManager(requestManager, new PersonCollection());
+        System.out.println("Клиент запущен! Порт: " + port);
+        String input;
+        do {
+            System.out.println("Введите команду: ");
+            if (!scanner.hasNextLine()) return;
+            input = scanner.nextLine();
+            try {
+                commandManager.existCommand(input);
+            } catch (Exception e) {
+                System.out.println("Ошибка");
+            }
+        } while (!input.equals("exit"));
+    }
 }
