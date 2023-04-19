e33bb5bba35e418f454a38ce90bcf33c090abaa2
Daria Shevchenko <daria.pinkfloyd@yandex.ru>
2023-04-19 22:12:15 +0300

    Change AddIfMin for server

--- src/main/java/client/commands/available/commands/AddIfMin.java
+++ src/main/java/client/commands/available/commands/AddIfMin.java
@@ -23,19 +23,47 @@ public class AddIfMin extends Command {
     @Override
     public void execute(String[] args) {
         try {
-            if (ExecuteScript.getFlag()) {
-                if (personCollection.addIfMinForScript(args[1])) {
-                    ExecuteScript.getPersonList().set(6, args[1]);
-                    personCollection.addPerson(ClientManager.createPersonFromScript(ExecuteScript.getPersonList()));
-                }
-            } else if (args.length > 2) {
+            //if (ExecuteScript.getFlag()) {
+            //if (PersonCollection.addIfMaxForScript(args[1])) {
+            //ExecuteScript.getPersonList().set(6, args[1]);
+            //PersonCollection.addPerson(ClientManager.createPersonFromScript(ExecuteScript.getPersonList()));
+            //}
+            //}
+            if (args.length > 2) {
                 System.out.println("Вы неправильно ввели команду");
             } else {
-                personCollection.addIfMin(args[1]);
+                PersonCollection personCollection = new PersonCollection();
+                personCollection.loadCollection();
+                if (!personCollection.toHeight(Integer.parseInt(args[1]))) {
+                    Scanner sc = new Scanner(System.in);
+                    Person newPerson = ClientManager.getNewPerson(sc);
+                    Request<Person> request = new Request<>(getName(), newPerson);
+                    CommandResult result = requestManager.sendRequest(request);
+
+                    if (result.status) {
+                        System.out.println((result.message));
+                        System.out.println("Ваш персонаж теперь в коллекции");
+                    } else
+                        System.out.println("Ошибка");
+                } else {
+                    System.out.println("Ваш персонаж не самый низкий!!");
+                }
+
             }
-        } catch (ArrayIndexOutOfBoundsException e) {
+        } catch (ArrayIndexOutOfBoundsException | JAXBException e) {
             System.out.println("Недостаточно аргументов, обратитесь к команде help");
+            e.printStackTrace();
         }
+    }
+
+    @Override
+    public String getName() {
+        return "add_if_min";
+    }
+
+    @Override
+    public String getDescription() {
+        return "добавить новый элемент в коллекцию, если его значение меньше, чем у наименьшего элемента этой коллекции";
     }
 }
 
