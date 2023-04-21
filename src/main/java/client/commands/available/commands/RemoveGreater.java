df3bc80e85de4fc1e24937a77209fc5f624755bf
Daria Shevchenko <daria.pinkfloyd@yandex.ru>
2023-04-21 16:33:21 +0300

    Made RemoveGreater for server

--- src/main/java/client/commands/available/commands/RemoveGreater.java
+++ src/main/java/client/commands/available/commands/RemoveGreater.java
@@ -1,27 +1,39 @@
 package client.commands.available.commands;
 
+import client.RequestManager;
 import client.commands.Command;
+import common.network.CommandResult;
+import common.network.Request;
 import server.PersonCollection;
+
+import javax.xml.bind.JAXBException;
 
 
 /**
  * Command remove_greater {element} : remove all elements from the collection that exceed the specified
  */
 public class RemoveGreater extends Command {
-    private final PersonCollection personCollection;
 
-    public RemoveGreater(PersonCollection personCollection) {
-        this.personCollection = personCollection;
+    public RemoveGreater(RequestManager requestManager) {
+        super(requestManager);
     }
 
     @Override
-    public void execute(String[] args) {
+    public void execute(String[] args) throws JAXBException {
         if (args.length != 2) {
             System.out.println("Вы неправильно ввели команду");
         } else {
-            personCollection.removeGreater(args[1]);
+            PersonCollection personCollection = new PersonCollection();
+            personCollection.loadCollection();
+            Request<String> request = new Request<>(getName(), args[1]);
+            CommandResult result = requestManager.sendRequest(request);
+            if (result.status) {
+                System.out.println((result.message));
+            } else
+                System.out.println("Ошибка");
         }
     }
+
     @Override
     public String getName() {
         return "remove_greater";
