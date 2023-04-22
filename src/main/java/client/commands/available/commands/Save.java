5079ada5c10f9bb86fceea08393b71d1029c0980
Daria Shevchenko <daria.pinkfloyd@yandex.ru>
2023-04-22 22:18:11 +0300

    Some fixes

--- src/main/java/client/commands/available/commands/Save.java
+++ src/main/java/client/commands/available/commands/Save.java
@@ -1,7 +1,10 @@
 package client.commands.available.commands;
 
+import client.RequestManager;
 import client.commands.Command;
 import client.commands.CommandManager;
+import common.network.CommandResult;
+import common.network.Request;
 import server.PersonCollection;
 
 import javax.xml.bind.JAXBException;
@@ -11,23 +14,26 @@ import java.io.IOException;
  * save : save the collection to a file
  */
 public class Save extends Command {
-    private final PersonCollection collection;
 
-    public Save(PersonCollection collection) {
-        this.collection = collection;
+
+    public Save(RequestManager requestManager) {
+        super(requestManager);
     }
 
 
     @Override
     public void execute(String[] args) throws JAXBException, IOException {
-        try {
-            if (args.length != 2) {
-                System.out.println("Вы неправильно ввели команду");
-            } else {
-                //collecton.save(args[1]);
-            }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            //collection.save(CommandManager.getFilelink());
+
+        if (args.length != 2) {
+            System.out.println("Вы неправильно ввели команду");
+        } else {
+            Request<String> request = new Request<>(getName(), null);
+            CommandResult result = requestManager.sendRequest(request);
+            if (result.status) {
+                System.out.println((result.message));
+            } else
+                System.out.println("Ошибка");
+
         }
     }
 
