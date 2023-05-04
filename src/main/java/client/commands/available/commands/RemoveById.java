bda3551ae46a9c9604ed50fb4f97eec2ffa2139c
Daria Shevchenko <daria.pinkfloyd@yandex.ru>
2023-05-04 20:02:41 +0300

    Fix remove by id for script

--- src/main/java/client/commands/available/commands/RemoveById.java
+++ src/main/java/client/commands/available/commands/RemoveById.java
@@ -19,31 +19,21 @@ public class RemoveById extends Command {
         super(requestManager);
     }
 
-    /**public static void remove_by_idForScript() {
-        try {
-            System.out.println("Введите Id персонажа, которого хотите удалить");
-            Scanner sc = new Scanner(System.in);
-            String a = sc.nextLine();
-            int ID = Integer.parseInt(a);
-            if (personCollection.existID(ID)) {
-                personCollection.removePerson(ID);
-                System.out.println("Персонаж удален");
-            } else {
-                System.out.println("Этого персонажа не существует");
-            }
-        } catch (NumberFormatException e) {
-            System.out.println("Вы неправильно ввели ID");
-        }
-    }*/
 
     @Override
     public void execute(String[] args) throws JAXBException {
-        if (ExecuteScript.getFlag()) {
-            //RemoveById.remove_by_idForScript();
-        } else if (args.length != 2) {
+        String id;
+        if (args.length > 2) {
             System.out.println("Вы неправильно ввели команду");
         } else {
-            Request<String> request = new Request<>(getName(), args[1], null);
+            if (args.length == 1) {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Введите ID");
+                id = scanner.nextLine();
+            } else {
+                id = args[1];
+            }
+            Request<String> request = new Request<>(getName(), id, null);
             CommandResult result = requestManager.sendRequest(request);
             if (result.status) {
                 System.out.println((result.message));
@@ -51,6 +41,7 @@ public class RemoveById extends Command {
                 System.out.println("Ошибка");
         }
     }
+
     @Override
     public String getName() {
         return "remove_by_id";
