c4d00cacd553c2b1b15e9081025262ba6dfc6447
Daria Shevchenko <daria.pinkfloyd@yandex.ru>
2023-05-06 14:25:46 +0300

    Fix exit

--- src/main/java/client/commands/available/commands/Exit.java
+++ src/main/java/client/commands/available/commands/Exit.java
@@ -1,17 +1,29 @@
 package client.commands.available.commands;
 
+import client.RequestManager;
 import client.commands.Command;
+import common.network.CommandResult;
+import common.network.Request;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * exit : terminate the program (without saving to a file)
  */
 public class Exit extends Command {
+    public Exit(RequestManager requestManager) {
+        super(requestManager);
+    }
     @Override
     public void execute(String[] args) {
         if (args.length > 1) {
             System.out.println("Вы неправильно ввели команду");
         } else {
-            System.out.println("Удачи");
+            Request<?> request = new Request<String>(getName(),null,null);
+            CommandResult result = requestManager.sendRequest(request);
+            if (result.status) {
+                System.out.println((result.message));
+            }
 
         }
     }
