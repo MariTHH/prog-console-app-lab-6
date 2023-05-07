5dad89238a54ad91e50bc0fd9262172c28ca646e
Daria Shevchenko <daria.pinkfloyd@yandex.ru>
2023-05-07 20:02:29 +0300

    Add javadoc

--- src/main/java/server/Executable.java
+++ src/main/java/server/Executable.java
@@ -4,6 +4,9 @@ package server;
 import common.network.CommandResult;
 import common.network.Request;
 
+/**
+ * Interface for commands
+ */
 public interface Executable {
     CommandResult execute(Request<?> request);
 }
